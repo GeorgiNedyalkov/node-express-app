15aac563dfb5bf9a0b913b4b48057ec84cef1b7c
Flu3nt <gnedyalkov94@gmail.com>
2023-02-16 11:25:13 +0200

    Install mongoose and connect to database

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-const port = 5000;
+const PORT = 5000;
 const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
 
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
@@ -11,6 +12,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(routes);
 
-app.listen(port, () => {
-  `Server is listening on port: ${port}`;
+const MONGO_URI = "mongodb://localhost:27017/node-express";
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGO_URI);
+
+app.listen(PORT, () => {
+  `Server is listening on port: ${PORT}`;
 });
