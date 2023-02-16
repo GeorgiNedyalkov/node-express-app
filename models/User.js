7c81760c5a49679ee7fb0afbc0998677ef498c63
Flu3nt <gnedyalkov94@gmail.com>
2023-02-16 14:16:24 +0200

    Add validation for mongoose model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    minLength: [5, "The username is too short"],
     required: true,
   },
   email: {
