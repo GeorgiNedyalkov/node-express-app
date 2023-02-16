6b1dda859c30397fed17abd3c4a1349446bd139b
Flu3nt <gnedyalkov94@gmail.com>
2023-02-16 11:20:07 +0200

    Add styles to login and register page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("pubic"));
+app.use(express.static("public"));
 app.use(routes);
 
 app.listen(port, () => {
